20c8089fcacfc98854b3b0235c1d3d7f1e477172
Connor Clark <connsect@outlook.com>
2022-11-10 14:39:02 -0500

    made several changes

--- javascript/louie.js
+++ javascript/louie.js
@@ -1,11 +1,13 @@
+
 // Louie Lopez Honor Roll Player
+const honorRoll = document.getElementById('honorRollApi');
+
 const loadHonorRollPlayer = () => {
    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2Cplayer&id=Y-A7pqw4Mxg&key=AIzaSyDuwQd5h_HvMexLOcKEHFbqMdLLwxpn728")
       .then((res) => {
         return res.json();
       })
       .then((data) => {
-        const honorRoll = document.getElementById('honorRollApi');
         honorRoll.innerHTML = data.items[0].player.embedHtml;
       })
       .catch((err) => {
@@ -14,20 +16,20 @@ const loadHonorRollPlayer = () => {
   };
   
   loadHonorRollPlayer();
-  
-  
+
   
   
   
   
   //Louie Lopez Again & Again Player
+  const again = document.getElementById('againApi');
+
   const loadAgainPlayer = () => {
     fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2Cplayer&id=j3QqchwDr10&key=AIzaSyDuwQd5h_HvMexLOcKEHFbqMdLLwxpn728")
       .then((res) => {
         return res.json();
       })
       .then((data) => {
-        const again = document.getElementById('againApi');
         again.innerHTML = data.items[0].player.embedHtml;
       })
       .catch((err) => {
@@ -36,3 +38,6 @@ const loadHonorRollPlayer = () => {
   };
   
   loadAgainPlayer(); 
+
+
+
